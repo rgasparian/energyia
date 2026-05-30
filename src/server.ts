fbad8fd5984715bb2367e7e9ea7878438df8c6d5
Rodolfo Gasparian <rgasparian@gmail.com>
2026-05-30 11:06:27 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -50,18 +50,19 @@ async function normalizeCatastrophicSsrResponse(response: Response): Promise<Res
 function rewriteForSubdomain(request: Request): Request {
   const url = new URL(request.url);
   const hostname = url.hostname;
-  if (hostname.startsWith("consultor.")) {
-    url.pathname = "/consultor" + url.pathname;
-    return new Request(url.toString(), request);
-  }
+
+  // matrix.energyia.club/joao → /matrix/joao
   if (hostname.startsWith("matrix.")) {
     url.pathname = "/matrix" + url.pathname;
     return new Request(url.toString(), request);
   }
+
+  // cliente.energyia.club/joao → /cliente/joao
   if (hostname.startsWith("cliente.")) {
     url.pathname = "/cliente" + url.pathname;
     return new Request(url.toString(), request);
   }
+
   return request;
 }
 
